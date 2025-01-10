bea3f4421278df65a4fe3b409b214ded9995d573
garagoz <atorayew@gmail.com>
2025-01-10 10:27:52 +0500

    gitlab installation added

---
+++ gitlab/gitlab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Recommended requirement
+#2 GB RAM or more
+#2 vCPUs or more
+
+sudo apt update -y
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl postfix
+
+# During the postfix installation, a configuration window will appear. Choose “Internet Site”
+
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt install gitlab-ce
+
+sudo vim /etc/gitlab/gitlab.rb
+# search this and change -> "external_url 'https://your_domain'"
+sudo gitlab-ctl reconfigure
