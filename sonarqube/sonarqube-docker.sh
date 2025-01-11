660e4887b6082e862be72a41ce1a44a064311266
garagoz <atorayew@gmail.com>
2025-01-11 23:40:31 +0500

    trivy.sh added

--- sonarqube/sonarqube-docker.sh
+++ sonarqube/sonarqube-docker.sh
@@ -17,6 +17,9 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo service docker restart
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
 
 # run sonar qube
 docker run --name sonarqube -p 9000:9000 -d sonarqube:lts-community
