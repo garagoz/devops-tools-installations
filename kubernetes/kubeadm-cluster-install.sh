44c48b8cac8cbe3b79c52b04f241928043f820f2
garagoz <atorayew@gmail.com>
2025-02-24 00:32:26 +0500

    github action added

--- kubernetes/kubeadm-cluster-install.sh
+++ kubernetes/kubeadm-cluster-install.sh
@@ -48,8 +48,8 @@ sudo systemctl restart containerd
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 # Attention: change kubeadm version whatever you want
-curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install kubelet kubeadm kubectl -y
 
